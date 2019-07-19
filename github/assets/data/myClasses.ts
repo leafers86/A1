e0d02a7c60853fa34d05144da9f8a4c90df1f225
Steve Markham <markhams@sheridancollege.ca>
2019-07-19 13:34:46 -0400

    Add files via upload

---
+++ github/assets/data/myClasses.ts
@@ -0,0 +1,6 @@
+import { ClassInfo } from '../../app/classInfo';
+
+export const MYCLASSES: ClassInfo[] = [
+  { cname: 'SYST24444', room: 'E103', dow: 'Friday', tod: '11-2' },
+  { cname: 'SYST35300', room: 'G225', dow: 'Tuesday', tod: '8-11' }
+]
